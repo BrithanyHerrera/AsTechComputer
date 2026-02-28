fed3d2537e5c7bba9c978bc14178280e6713f833
BrithanyHerrera <brithany.herrera6783@alumnos.udg.mx>
2026-02-28 00:46:13 -0600

    Refactor: se movieron las carpetas de componentes y se actualizaron rutas

---
+++ htdocs/public/index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Astech Computer</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&display=swap" rel="stylesheet">
+    
+    <link rel="stylesheet" href="css/static.css">
+    <link rel="icon" href="img/logoATC.ico" type="image/x-icon">
+</head>
+<body>
+
+    <header>
+        <nav>
+            <ul>
+                <li class="nav-link"><a href="servicios.php">Servicios</a></li>
+                <li class="nav-link"><a href="contacto.php">Contacto</a></li>
+                <li class="nav-link-P"><a href="cita.php">Agendar cita</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+        <div class="fondo">
+            <img src="img/logoATC.png" alt="Logo ATC" style="width:30%; margin-left:5%;">
+        </div>
+
+        <div class="infoAbUs">
+            <div class="texto">
+                <h2 class="titulo">¿Quienes somos?</h2>
+                <p>En Astech, somos una microempresa orgullosamente mexicana dedicada a transformar la relación de las personas con su tecnología. Entendemos que hoy en día, una computadora no es solo una herramienta; es tu oficina, tu centro de entretenimiento y tu conexión con el mundo.</p>
+                <p>Nacimos con la misión de ofrecer soluciones integrales en sistemas de información, combinando la precisión técnica con un trato honesto y transparente.</p>
+            </div>
+            <div class="imagen">
+                <div class="wrapper-irregular">
+                    <img src="img/revisandoCompu.jpg" alt="" class="Iprincipal">
+                </div>
+            </div>
+        </div>
+
+        <div class="infoMV">
+            <div class="mision">
+                <img src="img/mision.png" alt="Misión" style="width: 100%; height:100%;">
+                <div class="wrapperm">
+                    <h1>Misión</h1>
+                    <p>"Brindar soluciones tecnológicas integrales..."</p>
+                </div>
+            </div>
+            <div class="vision">
+                <img src="img/vision.png" alt="Visión" style="width: 100.7%; height:100%;">
+                <div class="wrapperv">
+                    <h1>Visión</h1>
+                    <p>"Consolidarnos como la microempresa líder..."</p>
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <footer>
+        </footer>
+
+    <script src="js/fuciones.js"></script>
+</body>
+</html>
