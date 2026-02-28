788dbf5b39d51242d35dc2a71263d6f419798286
BrithanyHerrera <brithany.herrera6783@alumnos.udg.mx>
2026-02-28 01:06:21 -0600

    FUNCIONAMIENTO CON LAS NUEVAS DIRECCIONES

---
+++ htdocs/joce.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Astech Computer</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="static/static.css">
+
+    <link rel="icon" href="static/img/logoATC.ico" type="image/x-icon">
+</head>
+
+<body>
+
+    <header>
+
+        <nav>
+            <ul>
+                <li class="nav-link"><a href="servicios.php">Servicios</a></li>
+                <li class="nav-link"><a href="contacto.php">Contacto</a></li>
+                <li class="nav-link-P"><a href="cita.php">Agendar cita</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+
+        <div class="fondo">
+            <img src="static/img/logoATC.PNG" alt="" style="width:30%; margin-left:5%;">
+        </div>
+
+        <!--<div class="contenedor">
+    <div class="caja">Caja 1</div>
+    <div class="caja">Caja 2</div>
+</div>-->
+
+
+
+        <div class="infoAbUs">
+            <div class="texto">
+                <h2 class="titulo">¿Quienes somos?</h2>
+                <p>En Astech, somos una microempresa orgullosamente mexicana dedicada a transformar la relación de las
+                    personas con su tecnología. Entendemos que hoy en día, una computadora no es solo una herramienta;
+                    es tu oficina, tu centro de entretenimiento y tu conexión con el mundo.</p>
+                <p>Nacimos con la misión de ofrecer soluciones integrales en sistemas de información, combinando la
+                    precisión técnica con un trato honesto y transparente</p>
+            </div>
+            <div class="imagen">
+                <div class="wrapper-irregular">
+                    <img src="static/img/revisandoCompu.jpg" alt="" class="Iprincipal">
+
+                </div>
+            </div>
+        </div>
+        <div class="infoMV">
+            <div class="mision">
+                <img src="static/img/mision.png" alt="" style="width: 100%; height:100%;">
+                <div class="wrapperm">
+                    <h1>Misión</h1>
+                    <p>"Brindar soluciones tecnológicas integrales en sistemas de información, mantenimiento y armado de
+                        equipos de cómputo, garantizando un servicio honesto, técnico y personalizado que permita a
+                        nuestros clientes optimizar su productividad y mantenerse conectados con lo que más les
+                        importa."</p>
+                </div>
+            </div>
+            <div class="vision">
+                <img src="static/img/vision.png" alt="" style="width: 100.7%; height:100%; ">
+                <div class="wrapperv">
+
+                    <h1>Visión</h1>
+                    <p>"Consolidarnos como la microempresa líder y de mayor confianza en nuestra región para el soporte
+                        técnico y ensamble de computadoras, siendo reconocidos por nuestra innovación constante, la
+                        calidad de nuestros procesos y el trato humano que nos distingue de las grandes corporaciones."
+                    </p>
+                </div>
+            </div>
+            <h2></h2>
+            <p></p>
+            <p></p>
+        </div>
+        <div class="info">
+            <h2></h2>
+            <p></p>
+            <p></p>
+        </div>
+
+    </main>
+
+    <footer>
+
+    </footer>
+
+    <script src="js/scripts.js"></script>
+</body>
+
+</html>
