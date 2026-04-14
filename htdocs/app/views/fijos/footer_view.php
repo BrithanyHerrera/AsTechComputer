835f457616be772d3041a577b5b3cc6e974fbd58
BrithanyHerrera <brithany.herrera6783@alumnos.udg.mx>
2026-04-14 10:01:17 -0600

    whats icon

--- htdocs/app/views/fijos/footer_view.php
+++ htdocs/app/views/fijos/footer_view.php
@@ -1,7 +1,8 @@
 <footer class="pie-pagina">
     <div class="contenedor-pie">
         <div class="seccion-logo">
-            <img src="<?php echo $ruta_prefijo; ?>public/img/logoATC.png" alt="AS TECH Logo" style="width: 160px; filter: brightness(0) invert(1);">
+            <img src="<?php echo $ruta_prefijo; ?>public/img/logoATC.png" alt="AS TECH Logo"
+                style="width: 160px; filter: brightness(0) invert(1);">
         </div>
         <div style="display: flex; justify-content: space-around; flex-wrap: wrap; gap: 30px;">
             <div class="fila-links">
@@ -17,7 +18,8 @@
                 <h4>Empresa</h4>
                 <ul>
                     <li><a href="#">Sobre Nosotros</a></li>
-                    <li><a href="<?php echo $ruta_prefijo; ?>app/controllers/convenios_controller.php">Convenios</a></li>
+                    <li><a href="<?php echo $ruta_prefijo; ?>app/controllers/convenios_controller.php">Convenios</a>
+                    </li>
                     <li><a href="<?php echo $ruta_prefijo; ?>app/controllers/login_controller.php">Operaciones</a></li>
                 </ul>
             </div>
@@ -25,7 +27,8 @@
                 <h4>Legal</h4>
                 <ul>
                     <li><a href="#">Política de privacidad</a></li>
-                    <li><a href="<?php echo $ruta_prefijo; ?>app/views/politica_cookies.php">Política de Cookies</a></li>
+                    <li><a href="<?php echo $ruta_prefijo; ?>app/views/politica_cookies.php">Política de Cookies</a>
+                    </li>
                 </ul>
             </div>
         </div>
@@ -38,6 +41,10 @@
                 <li><i class="fa-brands fa-tiktok"></i></li>
             </ul>
         </div>
+        <a href="https://wa.me/523221234567?text=Hola,%20vengo%20de%20la%20página%20web%20y%20necesito%20soporte%20técnico."
+            class="btn-whatsapp-flotante" target="_blank" rel="noopener noreferrer">
+            <i class="fa-brands fa-whatsapp"></i>
+        </a>
     </div>
     <div class="barra-derechos">
         <p>&copy; 2026 AS TECH COMPUTER. Todos los derechos reservados.</p>
