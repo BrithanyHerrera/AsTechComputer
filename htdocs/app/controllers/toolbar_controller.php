b91a776df11aea93c278a3e653bdb312fc13b6b8
BrithanyHerrera <brithany.herrera6783@alumnos.udg.mx>
2026-04-15 00:06:52 -0600

    analytics

--- htdocs/app/controllers/toolbar_controller.php
+++ htdocs/app/controllers/toolbar_controller.php
@@ -4,7 +4,25 @@
 // UBICACIÓN: app/controllers/toolbar_controller.php
 // =====================================================================
 
+$permitirAnaliticas = false; 
 
-// Cargamos la vista parcial del toolbar
+// 1. Buscamos el nombre EXACTO que usa tu JavaScript
+if (isset($_COOKIE['astech_preferencias_cookies'])) {
+    
+    // 2. Decodificamos el JSON
+    // (Tu JS usa encodeURIComponent, PHP lo decodifica en automático)
+    $cookies_estado = json_decode($_COOKIE['astech_preferencias_cookies'], true);
+    
+    // 3. Validación de los permisos
+    if (
+        is_array($cookies_estado) && 
+        isset($cookies_estado['analiticas']) && 
+        ($cookies_estado['analiticas'] === true || $cookies_estado['analiticas'] === 'true' || $cookies_estado['analiticas'] == 1)
+    ) {
+        $permitirAnaliticas = true;
+    }
+}
+
+// 4. Cargamos la vista de la barra de navegación
 require dirname(__DIR__) . '/views/fijos/toolbar_view.php';
 ?>
--- htdocs/app/views/fijos/toolbar_view.php
+++ htdocs/app/views/fijos/toolbar_view.php
@@ -1,17 +1,36 @@
+<?php 
+/**
+ * VISTA: toolbar_view.php
+ * Se encarga de mostrar la barra de navegación y el código de seguimiento (Google Analytics)
+ */
+
+if (isset($permitirAnaliticas) && $permitirAnaliticas): ?>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-862PM8JVQD"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-862PM8JVQD');
+    </script>
+<?php endif; ?>
+
 <header class="barra-herramientas">
     <div class="contenedor-logo">
         <a href="<?php echo $ruta_prefijo; ?>index.php">
-    <img src="<?php echo $ruta_prefijo; ?>public/img/Iso.png" class="logo-superior">
-</a>
-    
-        </div>
-    </a>
+            <img src="<?php echo $ruta_prefijo; ?>public/img/Iso.png" class="logo-superior" alt="AsTech Logo">
+        </a>
+    </div>
+
     <nav class="navegacion-principal">
         <ul class="lista-opciones">
-            <li><a  href="<?php echo $ruta_prefijo; ?>app/controllers/contacto_controller.php" class="enlace-opcion">Contacto</a></li>
-            <li><a  href="<?php echo $ruta_prefijo; ?>app/controllers/servicios_controller.php" class="enlace-opcion">Servicios</a></li>
-            <li><a href="<?php echo $ruta_prefijo; ?>app/controllers/citas_cliente_controller.php" class="enlace-opcion boton-subrayado">Agendar Cita</a></li>
-            <li><a href="<?php echo $ruta_prefijo; ?>app/controllers/politica_cookies_controller.php" class="enlace-opcion">Cookies</a></li>
+            <li><a href="<?php echo $ruta_prefijo; ?>app/controllers/contacto_controller.php" 
+                   class="enlace-opcion">Contacto</a></li>
+            <li><a href="<?php echo $ruta_prefijo; ?>app/controllers/servicios_controller.php" 
+                   class="enlace-opcion">Servicios</a></li>
+            <li><a href="<?php echo $ruta_prefijo; ?>app/controllers/citas_cliente_controller.php" 
+                   class="enlace-opcion boton-subrayado">Agendar Cita</a></li>
+            <li><a href="<?php echo $ruta_prefijo; ?>app/controllers/politica_cookies_controller.php" 
+                   class="enlace-opcion">Cookies</a></li>
         </ul>
     </nav>
 </header>
