237ca25feef148ffe9a37f8173baacd82979bcc0
Joceline2004 <joceline.delatorre6785@alumnos.udg.mx>
2026-04-26 21:45:09 -0600

    ruta

--- htdocs/app/config/config.php
+++ htdocs/app/config/config.php
@@ -1,3 +1,10 @@
 <?php
-define('BASE_URL', '/htdocs/');
+$protocolo = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? 'https' : 'http';
+$host = $_SERVER['HTTP_HOST'];
+
+// Detecta automáticamente la carpeta del proyecto
+$script = $_SERVER['SCRIPT_NAME']; 
+$path = str_replace(basename($script), '', $script);
+
+define('BASE_URL', $protocolo . '://' . $host . $path);
 ?>
