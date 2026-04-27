2c5561acdfbfe7c76f34bba742ced66ffff5a2f8
José Eduardo Flores Frausto <jose.flores5970@alumnos.udg.com>
2026-04-26 21:04:40 -0600

    base de datos

--- htdocs/app/config/config.php
+++ htdocs/app/config/config.php
@@ -1,3 +1,12 @@
 <?php
-define('BASE_URL', '/htdocs/');
+// 1. Detectar si es http o https
+$protocolo = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http';
+
+// 2. Detectar el host dinámicamente
+$host = $_SERVER['HTTP_HOST'];
+
+// 3. RUTA EXACTA 
+$carpeta_proyecto = '/'; 
+
+define('BASE_URL', $protocolo . '://' . $host . $carpeta_proyecto);
 ?>
