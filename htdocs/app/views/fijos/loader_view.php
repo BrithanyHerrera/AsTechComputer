99dce01f1c2c607a14e3e6047a5a5ee222f2c91b
Joceline2004 <joceline.delatorre6785@alumnos.udg.mx>
2026-05-04 10:46:23 -0600

    errores resueltos

--- htdocs/app/views/fijos/loader_view.php
+++ htdocs/app/views/fijos/loader_view.php
@@ -1,7 +1,7 @@
 <div id="pantalla-carga">
     <div class="loader-astech">
         <div class="circulo"></div>
-        <img src="<?php echo BASE_URL; ?>public/img/Iso.png" alt="Cargando..." class="logo-loader">
+        <img src="<?php echo BASE_URL; ?>public/img/isologotipo.png" alt="Cargando..." class="logo-loader">
     </div>
     <p class="texto-carga">Cargando sistema...</p>
 </div>
--- htdocs/index.php
+++ htdocs/index.php
@@ -1,4 +1,5 @@
-<?php // ======================================================== // CONTROLADOR: index.php (Página de Inicio Principal)
+<?php 
+// ======================================================== // CONTROLADOR: index.php (Página de Inicio Principal)
  // UBICACIÓN: Raíz del proyecto (AsTechComputer/index.php) 
  // ======================================================== // Cargamos la vista de inicio.
  require_once __DIR__ . '/app/config/config.php';
